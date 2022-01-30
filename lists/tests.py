fa2f8c56b162629e501c29b866bf17eb7a5f4c29
Bárbara Gomes <barbaragomes@dcc.ufmg.br>
2022-01-30 22:30:25 +0100

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- lists/tests.py
+++ lists/tests.py
@@ -44,6 +44,9 @@ class ItemModelTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
